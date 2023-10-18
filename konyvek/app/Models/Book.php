5fdb5c59504efaefdd8bbe215ad1c59f1af304bf
terenyijanos96 <terenyi.janos96@gmail.com>
2023-10-18 18:32:34 +0200

    adatbázis szerekezetének kialakítása

---
+++ konyvek/app/Models/Book.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    use HasFactory;
+
+    public $primaryKey = 'book_id';
+
+    protected $fillable = [
+        'author',
+        'title',
+        'pieces',
+    ];
+}
